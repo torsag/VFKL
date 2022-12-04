a7026bafe15871170d4ca7bd00797e94804ce7d3
Dhanalakshmi Gopalswamy <d.a.gopalswamy@accenture.com>
2022-12-04 22:18:41 +0100

    Added procedures to the clean db deploy script

--- deployment/cleandbdeploy.sql
+++ deployment/cleandbdeploy.sql
@@ -468,3 +468,98 @@ VALUES
 	  (19,'Oahppopládna dilev láhtjá váj oahppe bessi adnet ja åvddånahttet fága vuodulasj tjehpudagájt',4155),
 	  (20,'Oahpponævon la buorre progresjåvnnå fága máhtudakulmij gáktuj',4155),
 	  (21,'Oahpponævvo doarjju oahppe ja åhpadiddje bargov åhpadattijn árvustallamijn',4155);
+
+
+-- procedures
+
+-- PROCEDURE: userprofile.insert_user(character varying, character varying)
+
+-- DROP PROCEDURE IF EXISTS userprofile.insert_user(character varying, character varying);
+
+CREATE OR REPLACE PROCEDURE userprofile.insert_user(
+	IN feideid character varying,
+	IN username character varying)
+LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+	IF NOT EXISTS(SELECT * FROM userprofile.userprofile where feide_id =feideid) THEN
+		INSERT INTO userprofile.userprofile (feide_id, name) VALUES (feideid,username);
+	END IF;
+END
+$BODY$;
+
+-- PROCEDURE: invitations.insert_invitation(integer, character varying, character varying, character varying, character varying, character varying, character varying, integer, character varying, character varying, character varying)
+
+-- DROP PROCEDURE IF EXISTS invitations.insert_invitation(integer, character varying, character varying, character varying, character varying, character varying, character varying, integer, character varying, character varying, character varying);
+
+CREATE OR REPLACE PROCEDURE invitations.insert_invitation(
+	IN userid integer,
+	IN gvid character varying,
+	IN frist character varying,
+	IN laeremiddel character varying,
+	IN laereplan character varying,
+	IN mottakereposter character varying,
+	IN opprettetdato character varying,
+	IN assessmenttypeid integer,
+	IN laereplankode character varying,
+	IN bortvalgtsporsmaal character varying,
+	IN laeremiddelleverandor character varying)
+LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+IF NOT EXISTS(SELECT * FROM invitations.invitations where gv_id = gvid) THEN
+INSERT INTO invitations.invitations (user_id_fk, gv_id, frist, laeremiddel, laereplan, mottaker_eposter, opprettet_dato,assessmenttype_id_fk,laereplankode,bortvalgtsporsmaal, laeremiddel_leverandor)
+VALUES (userid, gvid, frist, laeremiddel, laereplan, mottakereposter,opprettetdato,assessmenttypeid,laereplankode,bortvalgtsporsmaal, laeremiddelleverandor);
+END IF;
+END
+$BODY$;
+
+-- PROCEDURE: assessment.insert_assessment(integer, character varying, character varying, character varying, timestamp without time zone, character varying, character varying)
+
+-- DROP PROCEDURE IF EXISTS assessment.insert_assessment(integer, character varying, character varying, character varying, timestamp without time zone, character varying, character varying);
+
+CREATE OR REPLACE PROCEDURE assessment.insert_assessment(
+	IN userid integer,
+	IN teachingaid character varying,
+	IN instanceid character varying,
+	IN groupassessmentid character varying,
+	IN createddatetime timestamp without time zone,
+	IN usercomments character varying,
+	IN teachingaidsupplier character varying)
+LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+IF NOT EXISTS(SELECT * FROM assessment.assessment where instance_id = instanceid) THEN
+INSERT INTO assessment.assessment (user_id_fk, teachingaid,instance_id, groupassessment_id_fk, created_datetime, user_comments, teachingaid_supplier) VALUES (userid, teachingaid,instanceid,groupassessmentid, createddatetime, usercomments, teachingaidsupplier);
+END IF;
+END
+$BODY$;
+
+-- PROCEDURE: assessment.insert_answers(integer, integer, integer, character varying)
+
+-- DROP PROCEDURE IF EXISTS assessment.insert_answers(integer, integer, integer, character varying);
+
+CREATE OR REPLACE PROCEDURE assessment.insert_answers(
+	IN assessmentid integer,
+	IN questionid integer,
+	IN answertypeid integer,
+	IN reason character varying)
+LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN
+	IF NOT EXISTS(SELECT * FROM assessment.answers where assessment_id_fk =assessmentid and question_id_fk = questionid) THEN
+		INSERT INTO assessment.answers (assessment_id_fk, question_id_fk,answertype_id_fk,reason) VALUES (assessmentid, questionid,answertypeid,reason);
+	END IF;
+END
+$BODY$;
+
+-- PROCEDURE: assessment.get(character varying)
+
+-- DROP PROCEDURE IF EXISTS assessment.get(character varying);
+
+CREATE OR REPLACE PROCEDURE assessment.get(
+	instanceid character varying)
+LANGUAGE 'sql'
+AS $BODY$
+SELECT * FROM assessment where instance_id=instanceid;
+$BODY$;
